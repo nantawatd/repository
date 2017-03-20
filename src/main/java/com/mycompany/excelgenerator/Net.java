7224b059729cbd87f9861629eeca809bdc14bfb6
nantawatd <nantawat.d88@gmail.com>
2017-03-20 15:31:23 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/excelgenerator/Net.java
@@ -0,0 +1,19 @@
+package com.mycompany.excelgenerator;
+
+public class Net {
+	private String wide;
+	private String height;
+	
+	public String getWide() {
+		return wide;
+	}
+	public void setWide(String wide) {
+		this.wide = wide;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+}
