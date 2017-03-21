e7beab5d5ec2ff4ccbb1a20ad14a4358a199f184
Nantawat Dejcharoen <nantawat.dejcharoen@allianz.com>
2017-03-21 17:20:39 +0700

    add table

--- ExcelGenerator/src/main/java/com/mycompany/ui/StartFrame.java
+++ ExcelGenerator/src/main/java/com/mycompany/ui/StartFrame.java
@@ -15,9 +15,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 import org.jxls.common.Context;
 import org.jxls.util.JxlsHelper;
 
@@ -60,12 +62,12 @@ public class StartFrame extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         customerTxt = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
-        sellerSelect = new javax.swing.JComboBox<>();
+        sellerSelect = new javax.swing.JComboBox<String>();
         jPanel5 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        reportTable = new javax.swing.JTable();
+        addButton = new javax.swing.JButton();
+        removeButton = new javax.swing.JButton();
         exportExcel = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -183,39 +185,52 @@ public class StartFrame extends javax.swing.JFrame {
 
         jPanel3.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, 270, 350));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        reportTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "เลขที่บิล", "เลขใบเสนอราคา", "ชื่อลูกค้า", "ชื่อเซลล์", "วันที่สั่ง", "วันที่รับ"
             }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(reportTable);
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
         jPanel5Layout.setHorizontalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel5Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 278, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 2, Short.MAX_VALUE))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 440, Short.MAX_VALUE)
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 350, Short.MAX_VALUE)
         );
 
-        jPanel3.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 80, 280, 350));
+        jPanel3.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 80, 440, 350));
 
-        jButton1.setText("Add >>");
-        jPanel3.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 180, 100, -1));
+        addButton.setText("Add >>");
+        addButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addButtonActionPerformed(evt);
+            }
+        });
+        jPanel3.add(addButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 180, 100, -1));
 
-        jButton2.setText("<< Remove");
-        jPanel3.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 230, 100, -1));
+        removeButton.setText("<< Remove");
+        removeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                removeButtonActionPerformed(evt);
+            }
+        });
+        jPanel3.add(removeButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 230, 100, -1));
 
         exportExcel.setText("Export Report");
         exportExcel.addActionListener(new java.awt.event.ActionListener() {
@@ -231,7 +246,7 @@ public class StartFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 708, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 871, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -245,15 +260,12 @@ public class StartFrame extends javax.swing.JFrame {
         try {
             Order order = new Order();
             order.setBillNumber(billNumberTxt.getText());
-            order.setCustomerName(customerTxt.getText());
             order.setQuotationId(quotationIdTxt.getText());
+            order.setCustomerName(customerTxt.getText());
             order.setSellerName(sellerSelect.getSelectedItem().toString());
-           
-            SimpleDateFormat formater = new SimpleDateFormat("dd-MM-yyyy");
-            formater.format(orderDate.getDate());
             
             order.setOrderDate(formater.format(orderDate.getDate()));
-            order.setReceiveDate( formater.format(receiveDate.getDate()));
+            order.setReceiveDate(formater.format(receiveDate.getDate()));
             
             
             createReport(order);
@@ -261,6 +273,30 @@ public class StartFrame extends javax.swing.JFrame {
             Logger.getLogger(StartFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_exportExcelActionPerformed
+
+    private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
+        DefaultTableModel model = (DefaultTableModel) reportTable.getModel();
+        model.addRow(new Object[]{billNumberTxt.getText(), 
+                     quotationIdTxt.getText(), 
+                     customerTxt.getText(), 
+                     sellerSelect.getSelectedItem().toString(), 
+                     formater.format(orderDate.getDate()), 
+                     formater.format(receiveDate.getDate())}
+        );
+    }//GEN-LAST:event_addButtonActionPerformed
+
+    private void removeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_removeButtonActionPerformed
+        DefaultTableModel model = (DefaultTableModel) reportTable.getModel();
+//        int selectedRow = reportTable.getSelectedRow();
+//        if(selectedRow != -1) {
+//            model.removeRow(selectedRow);
+//        }
+        int[] indices = reportTable.getSelectedRows();
+        Arrays.sort(indices);
+        for (int i = indices.length - 1; i >= 0; i--) {
+            model.removeRow(indices[i]);
+        }
+    }//GEN-LAST:event_removeButtonActionPerformed
 
     private void createReport(Order order) throws FileNotFoundException, IOException{
         String srcFilePath = "target/complete.xls";
@@ -326,12 +362,12 @@ public class StartFrame extends javax.swing.JFrame {
         });
     }
 
+    private SimpleDateFormat formater = new SimpleDateFormat("dd-MM-yyyy");
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton addButton;
     private javax.swing.JTextField billNumberTxt;
     private javax.swing.JTextField customerTxt;
     private javax.swing.JToggleButton exportExcel;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -346,10 +382,11 @@ public class StartFrame extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     private org.jdesktop.swingx.JXDatePicker orderDate;
     private javax.swing.JTextField quotationIdTxt;
     private org.jdesktop.swingx.JXDatePicker receiveDate;
+    private javax.swing.JButton removeButton;
+    private javax.swing.JTable reportTable;
     private javax.swing.JComboBox<String> sellerSelect;
     // End of variables declaration//GEN-END:variables
 }
