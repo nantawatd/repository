99ce16b8e6b25cd8c3e769c5b61d7ee6fa800103
Nantawat Dejcharoen <nantawat.dejcharoen@allianz.com>
2017-03-21 12:34:38 +0700

    ex

---
+++ ExcelGenerator/src/main/java/com/mycompany/excelgenerator/dto/Net.java
@@ -0,0 +1,22 @@
+package com.mycompany.excelgenerator.dto;
+
+public class Net {
+    private String wide;
+    private String height;
+
+    public String getWide() {
+        return wide;
+    }
+
+    public void setWide(String wide) {
+        this.wide = wide;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+}
